63cc6429279133924a2967a3be0b8e2acf5b8e52
Rodrigo arruda <arruda384digo@gmail.com>
2018-12-04 13:04:32 -0300

    atualizando

--- src/main/java/com/pointsistemas/cursomc/domain/Categoria.java
+++ src/main/java/com/pointsistemas/cursomc/domain/Categoria.java
@@ -1,21 +1,27 @@
 package com.pointsistemas.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
-public class Categoria  implements Serializable{
+public class Categoria implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	private List<Produto> produtos = new ArrayList<>();
 	
+	@ManyToMany(mappedBy= "categorias")
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -58,7 +64,7 @@ public class Categoria  implements Serializable{
 	}
 
 	public Categoria() {
-		
+
 	}
 
 	public Categoria(Integer id, String nome) {
@@ -66,6 +72,13 @@ public class Categoria  implements Serializable{
 		this.id = id;
 		this.nome = nome;
 	}
-	
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 }
